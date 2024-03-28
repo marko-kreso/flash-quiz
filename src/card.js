c5b3ca4e1ae12469dca5c137ddcaf090d4ecf3d7
marko-kreso <43715847+marko-kreso@users.noreply.github.com>
2024-03-28 11:18:27 -0700

    Add files via upload

--- src/card.js
+++ src/card.js
@@ -1,8 +1,114 @@
-function CardHtml(card){
-    return `
-        <div> 
-            Question: ${card.question}
-            Answer: ${card.answers}
-        </div>
-    `
+import Papa from 'papaparse';
+/**
+ * @typedef {Object} Card
+ * @property {string} question
+ * @property {string} answer
+ * @property {Array<string>} incorrect_answers
+ */
+
+/**
+ * @typedef {Map<string,string>} QA - Maps a question to an answer
+ */
+/**
+ * @callback csvCallback
+ * @param {Array<Card>}
+ */
+
+/**
+ * 
+ * @template A
+ * @param {Array<A>} arr 
+ * @param {number} n 
+ * @returns {Array<A>} Sampled array
+ */
+function RandomSampleWithoutReplacement(arr, n){
+
+    /** @type {Array<A>} */
+    const out = []
+    arr = [...arr]
+    console.log(arr)
+
+    for(let i = 0; i < n; i++){
+
+        let rand_i = Math.floor(Math.random()*arr.length)
+        out.push(...arr.splice(rand_i, 1)) 
+    }
+
+    console.log(out)
+    return out
 }
+
+
+export function Shuffle(arr){
+    for(let i =0; i < arr; arr.length){
+        let rand_i = Math.floor(Math.random()*(arr.length-1))
+        let tmp = arr[i]
+        arr[i] = arr[rand_i]
+        arr[rand_i]=tmp
+    }
+    return arr
+}
+export default{
+
+    /**
+     * 
+     * @param {QA} qa 
+     * @returns {Array<Card>}
+     */
+    CreateCards(qa){
+        /** @type {Array<Card>} */
+        let cards = []
+
+        const answers = new Set([...qa.values()])
+        qa.forEach((answer,question)=>{
+            /** @type {Card} */
+            let card = {
+                question: question,
+                answer: answer
+            }
+
+            if(answer === "true"){
+                card.incorrect_answers = ["false"]
+            }
+            else if(answer === "false"){
+                card.incorrect_answers = ["true"]
+            }else{
+                answers.delete(answer)
+                card.incorrect_answers = RandomSampleWithoutReplacement(Array.from(answers), 3)
+                answers.add(answer)
+            }
+
+            cards.push(
+                card
+            )
+        })
+
+        return cards
+    },
+    /**
+     * 
+     * @param {File} file 
+     * @param {csvCallback} cb 
+     */
+    ReadCsv(file, cb){
+        /** @type {QA} */
+        const qa = new Map()
+        Papa.parse(file,{
+            header: true,
+            dynamicTyping: true,
+            /**
+             * 
+             * @param {Array<{name: string, age: number}>} data
+             * 
+             */
+            complete: ({data})=>{
+                /**@type {QA} */
+                for(let result of data){
+                    qa.set(result.name,result.age)
+                }
+                cb(this.CreateCards(qa))
+            }
+        })
+    },
+}
+
--- src/index.js
+++ src/index.js
@@ -1,78 +1,15 @@
+import Alpine from 'alpinejs';
+import QuizCard from './card.js'
+import { Shuffle } from './card.js';
 
 
-/**
- * 
- * @param {Array<string>} arr 
- * @param {string} n 
- * @returns {Array<string>} Sampled array
- */
-function RandomSampleWithoutReplacement(arr, n){
-
-    /** @type {Array<number>} */
-    const out = []
-    arr = [...arr]
-
-    for(let i = 0; i < n; i++){
-        out = [...out, ...arr.splice(Math.floor(Math.random()*arr.length), 1)]
-    }
-
-    return out
-}
-
-/**
- * @typedef {Map<string,string>} QA - Maps a question to an answer
- */
-
-/**
- * @returns {QA}
- */
-function ReadCsv(){
-
-}
-
-
-/**
- * 
- * @param {QA} qa 
- * @returns {Array<Card>}
- */
-function CreateCards(qa){
-    /** @type {Array<Card>} */
-    let cards = []
-
-    const answers = new Set([...qa.values()])
-    qa.forEach((question,answer)=>{
-        /** @type {Card} */
-        let card = {
-            question: question,
-            answer: answer
-        }
-
-        if(answer === "true"){
-            card.incorrect_answers = ["false"]
-        }
-        else if(answer === "false"){
-            card.incorrect_answers = ["true"]
-        }else{
-            card.incorrect_answers = RandomSampleWithoutReplacement(Array.from(answers.difference(new Set([answer]))), 3)
-        }
-
-        cards.push(
-            card
-        )
+Alpine.data(
+    "util", ()=>({
+        CreateCards: QuizCard.CreateCards,
+        ReadCsv: QuizCard.ReadCsv,
+        Shuffle: Shuffle
     })
-
-    return cards
-}
+)
 
 
-/**
- * @typedef {Object} Card
- * @property {string} question
- * @property {string} answer
- * @property {Array<string>} incorrect_answers
- */
-
-
-/** @type {Card} */
-let card;
+Alpine.start()
