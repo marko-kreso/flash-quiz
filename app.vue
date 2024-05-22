39217d81a8a9e19df01c56416b5034620191d44b
Marko Kreso <mkr@blazmkr.rinconres.com>
2024-05-22 14:26:13 -0700

    changed column layout

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="flex-row flex h-screen">
+  <div class="flex-col flex h-screen">
+    <div class="h-16 bg-slate-300 sticky z-10 w-full top-0 flex-shrink-0">
+      title
+    </div>
+    <div class="flex-row flex flex-1 min-h-0">
       <div class="basis-48 flex flex-col">
-        <div class="bg-blue-300 basis-32"></div>
         <div class="bg-orange-300 flex-1">
           <nav class="h-full flex flex-col">
             <ul class="flex flex-col flex-1">
@@ -15,10 +18,7 @@
           </nav>
         </div>
       </div>
-      <div class="flex-1 basis-11/12 flex-col overflow-scroll">
-        <div class="h-16 bg-slate-300 sticky z-10 w-full top-0">
-          title
-        </div>
+      <div class="flex-1 basis-11/12 flex-col overflow-scroll flex">
         <div class="bg-slate-50">
           <CardList id="hello" url="https://test.com" v-slot="slotprops">
             <QuizCard>
@@ -28,5 +28,6 @@
           </CardList>
         </div>
       </div>
+    </div>
     </div>
 </template>
