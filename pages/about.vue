9cb596ad807500cd9125bffe25d1daa48d9d11d2
Marko Kreso <mkr@blazmkr.rinconres.com>
2024-06-03 13:09:52 -0700

    added about page

---
+++ pages/about.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="flex flex-col w-1/2 justify-center">
+    <div class="flex flex-row justify-center">
+      <h1>About</h1>
+    </div>
+    <p>
+    GigaQuiz seeks to be a better alternative to Quizlet and its name reflects that, which is why GigaQuiz's name is prefixed with "Giga" like in the word Gigachad whereas Quizlet's is suffixed with "let" in manlet.
+    GigaQuiz was created because I wanted to create a free and performant alternative to Quizlet. 
+    I found Quizlet too be painfully slow, and wanted a soydev free version of it.
+    GigaQuiz is being developed solo and on my free time.
+    I hope that this website can serve as a tool to help others study and am open to feedback. I would prefer to keep the site
+    ad-free if possible.
+
+    </p>
+
+    <h2 class="flex flex-row justify-center">Code of Ethics</h2>
+    <p>
+      Gigaquiz follows in sqlite's lead and purposes <span class="font-bold">The Rule</span> for all contributers to follow:
+    </p>
+    <ol class="list-decimal ml-16 text-lg font-bold">
+  <li> First of all, love the Lord God with your whole heart, your whole soul, and your whole strength.</li>
+  <li>Then, love your neighbor as yourself.</li>
+  <li>Do not murder.</li>
+  <li>Do not commit adultery.</li>
+  <li>Do not steal.</li>
+  <li>Do not covet.</li>
+  <li>Do not bear false witness.</li>
+  <li>Honor all people.</li>
+  <li>Do not do to another what you would not have done to yourself.</li>
+  <li>Deny oneself in order to follow Christ.</li>
+  <li>Chastise the body.</li>
+  <li>Do not become attached to pleasures.</li>
+  <li>Love fasting.</li>
+  <li>Relieve the poor.</li>
+  <li>Clothe the naked.</li>
+  <li>Visit the sick.</li>
+  <li>Bury the dead.</li>
+  <li>Be a help in times of trouble.</li>
+  <li>Console the sorrowing.</li>
+  <li>Be a stranger to the world's ways.</li>
+  <li>Prefer nothing more than the love of Christ.</li>
+  <li>Do not give way to anger.</li>
+  <li>Do not nurse a grudge.</li>
+  <li>Do not entertain deceit in your heart.</li>
+  <li>Do not give a false peace.</li>
+  <li>Do not forsake charity.</li>
+  <li>Do not swear, for fear of perjuring yourself.</li>
+  <li>Utter only truth from heart and mouth.</li>
+  <li>Do not return evil for evil.</li>
+  <li>Do no wrong to anyone, and bear patiently wrongs done to yourself.</li>
+  <li>Love your enemies.</li>
+  <li>Do not curse those who curse you, but rather bless them.</li>
+  <li>Bear persecution for justice's sake.</li>
+  <li>Be not proud.</li>
+  <li>Be not addicted to wine.</li>
+  <li>Be not a great eater.</li>
+  <li>Be not drowsy.</li>
+  <li>Be not lazy.</li>
+  <li>Be not a grumbler.</li>
+  <li>Be not a detractor.</li>
+  <li>Put your hope in God.</li>
+  <li>Attribute to God, and not to self, whatever good you see in yourself.</li>
+  <li>Recognize always that evil is your own doing, and to impute it to yourself.</li>
+  <li>Fear the Day of Judgment.</li>
+  <li>Be in dread of hell.</li>
+  <li>Desire eternal life with all the passion of the spirit.</li>
+  <li>Keep death daily before your eyes.</li>
+  <li>Keep constant guard over the actions of your life.</li>
+  <li>Know for certain that God sees you everywhere.</li>
+  <li>When wrongful thoughts come into your heart, dash them against Christ immediately.</li>
+  <li>Disclose wrongful thoughts to your spiritual mentor.</li>
+  <li>Guard your tongue against evil and depraved speech.</li>
+  <li>Do not love much talking.</li>
+  <li>Speak no useless words or words that move to laughter.</li>
+  <li>Do not love much or boisterous laughter.</li>
+  <li>Listen willingly to holy reading.</li>
+  <li>Devote yourself frequently to prayer.</li>
+  <li>Daily in your prayers, with tears and sighs, confess your past sins to God, and amend them for the future.</li>
+  <li>Fulfill not the desires of the flesh; hate your own will.</li>
+  <li>Obey in all things the commands of those whom God has placed in authority over you even though they (which God forbid) should act otherwise, mindful of the Lord's precept, "Do what they say, but not what they do."</li>
+  <li>Do not wish to be called holy before one is holy; but first to be holy, that you may be truly so called.</li>
+  <li>Fulfill God's commandments daily in your deeds.</li>
+  <li>Love chastity.</li>
+  <li>Hate no one.</li>
+  <li>Be not jealous, nor harbor envy.</li>
+  <li>Do not love quarreling.</li>
+  <li>Shun arrogance.</li>
+  <li>Respect your seniors.</li>
+  <li>Love your juniors.</li>
+  <li>Pray for your enemies in the love of Christ.</li>
+  <li>Make peace with your adversary before the sun sets.</li>
+  <li>Never despair of God's mercy. </li>
+      </ol>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+h1{
+  font-weight: bold;
+  font-size: x-large;
+}
+h2{
+  font-weight: bold;
+  font-size: large;
+}
+h3{
+  font-weight: bold;
+  font-size: large;
+}
+</style>
