0e85762281243d6db279466f79eb06f876ce5cc2
Marko Kreso <markokreso@gmail.com>
2024-06-08 14:15:04 -0700

    fixed migration file need to do further testing

--- sql/migrations/0001_init.sql
+++ sql/migrations/0001_init.sql
@@ -1,4 +1,5 @@
 -- +migrate Up
+
 CREATE TABLE IF NOT EXISTS users(
     username TEXT PRIMARY KEY
     ,email TEXT UNIQUE NOT NULL
@@ -19,38 +20,49 @@ CREATE TABLE IF NOT EXISTS social_users(
     ,username TEXT UNIQUE NOT NULL REFERENCES users(username) ON DELETE CASCADE NOT NULL
     ,PRIMARY KEY (social_id, social_media)
 );
+
+CREATE EXTENSION LTREE;
 CREATE TABLE folders(
-    path LTREE PRIMARY KEY,
-)
+    path LTREE PRIMARY KEY
+);
 CREATE INDEX folder_path_gist_idx ON folders USING GIST (path gist_ltree_ops(siglen=128));
 
+CREATE FUNCTION immutable_array_to_string(arr ANYARRAY, sep TEXT)
+    RETURNS text
+    AS $$SELECT array_to_string(arr, sep);$$
+    LANGUAGE SQL
+    IMMUTABLE
+  ;
+
+-- TEST if index will work later
 CREATE TABLE IF NOT EXISTS card_sets(
     path LTREE GENERATED ALWAYS AS(
         folder || text2ltree(name)
     ) STORED PRIMARY KEY
     ,folder LTREE NOT NULL REFERENCES folders(path) ON DELETE CASCADE
-    , name TEXT
+    , name TEXT NOT NULL
     ,username TEXT REFERENCES users(username) NOT NULL
     ,questions TEXT[] NOT NULL
     ,answers TEXT[] NOT NULL
     ,search TSVECTOR GENERATED ALWAYS AS(
         SETWEIGHT(TO_TSVECTOR('english', name), 'A')
-        || SETWEIGHT(TO_TSVECTOR('english', ARRAY_TO_STRING(questions, ' ')), 'B')
-        || SETWEIGHT(TO_TSVECTOR('english', ARRAY_TO_STRING(answers, ' ')), 'C')
+        || SETWEIGHT(TO_TSVECTOR('english', IMMUTABLE_ARRAY_TO_STRING(questions, ' ')::text), 'B')
+        || SETWEIGHT(TO_TSVECTOR('english', IMMUTABLE_ARRAY_TO_STRING(answers, ' ')::text), 'C')
     ) STORED
     ,created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE INDEX card_path_gist_idx ON card_sets USING GIST (path gist_ltree_ops(siglen=128));
+CREATE INDEX card_text_gist_idx ON card_sets USING GIN (search);
 
 CREATE TABLE IF NOT EXISTS user_favorites(
-    username TEXT REFERENCES users(username) ON DELETE CASCADE NOT NULL 
-    ,set_user TEXT NOT NULL REFERENCES card_sets(username)
-    ,path TEXT NOT NULL REFERENCES card_sets(path)
+    username TEXT REFERENCES users(username) ON DELETE CASCADE NOT NULL
+    ,set_user TEXT NOT NULL REFERENCES users(username)
+    ,path LTREE NOT NULL REFERENCES card_sets(path) 
     ,PRIMARY KEY(username, set_user, path)
 );
 
-CREATE INDEX IF NOT EXISTS favorite_idx ON UserFavorites(set_user, path);
+CREATE INDEX IF NOT EXISTS favorite_idx ON user_favorites(set_user, path);
 
 CREATE TABLE IF NOT EXISTS password_change_requests(
     id TEXT
@@ -65,11 +77,14 @@ CREATE TABLE IF NOT EXISTS password_change_requests(
 DROP INDEX IF EXISTS favorite_idx;
 DROP TABLE IF EXISTS user_favorites;
 DROP INDEX IF EXISTS card_path_gist_idx;
+DROP INDEX IF EXISTS card_text_gist_idx; 
 DROP TABLE IF EXISTS card_sets;
-DROP INDEX IF EXSITS folder_path_gist_idx;
+DROP INDEX IF EXISTS folder_path_gist_idx;
 DROP TABLE IF EXISTS folders;
 DROP TABLE IF EXISTS social_users;
 DROP TABLE IF EXISTS passwords;
 DROP INDEX IF EXISTS email_idx;
+DROP TABLE IF EXISTS password_change_requests;
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS password_change_request;
+DROP EXTENSION IF EXISTS LTREE;
+DROP FUNCTION IF EXISTS immutable_array_to_string;
